1cd35d682fa647b0da1839d27da012aa9cd53ab9
Kapil Vaswani <kapilv@microsoft.com>
2025-03-20 23:02:56 +0000

    change KMS

--- deployment-scripts/azure/offer-service/environment/demo/terraform/offer.tf
+++ deployment-scripts/azure/offer-service/environment/demo/terraform/offer.tf
@@ -26,7 +26,7 @@ locals {
   registry_path  = "depa-inferencing/azure"
   image_tag      = "prod-4.3.0.0"
   kv_image_tag   = "prod-1.0.0.0"
-  kms_url        = "https://depa-inferencing-kms.northeurope.azurecontainer.io:8000"
+  kms_url        = "https://depa-inferencing-kms.centralindia.cloudapp.azure.com"
 }
 
 module "offer" {
