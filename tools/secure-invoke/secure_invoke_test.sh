9b733923ad8da85fc7beb3b9dfeb866a18342594
pavankad <azureuser@privacy-sandbox.512ruas1zb4ulla3bilrecefua.bx.internal.cloudapp.net>
2025-05-15 11:55:10 +0000

    updated for http support

--- tools/secure-invoke/secure_invoke_test.sh
+++ tools/secure-invoke/secure_invoke_test.sh
@@ -17,5 +17,8 @@ echo "  Host Requests DIR: $HOST_REQUESTS_DIR"
 echo "  KMS Host: $KMS_HOST"
 echo "  Buyer Host: $BUYER_HOST"
 echo "  Retries: $RETRIES"
+echo "  Target Service: $TARGET_SERVICE"
+echo "  HEADERS: $HEADERS"
+echo "  Insecure Mode: $insecure"
 
 docker compose -f "$REPO_ROOT/docker-compose.yml" up secure-invoke
