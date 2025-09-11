0dec870124f7805bfa8cb87dfd2dbbcca35e6316
Kapil Vaswani <46554948+kapilvgit@users.noreply.github.com>
2025-09-11 16:18:04 +0530

    switch to prod images in terraform and policies (#39)
    * test prod images
    
    * prod images

--- deployment-scripts/azure/offer-service/environment/demo/terraform/offer.tf
+++ deployment-scripts/azure/offer-service/environment/demo/terraform/offer.tf
@@ -25,8 +25,8 @@ locals {
 
   image_registry = "ispirt.azurecr.io"
   registry_path  = "depa-inferencing/azure"
-  image_tag      = "nonprod-4.8.0.0"
-  kv_image_tag   = "nonprod-1.2.0.0"
+  image_tag      = "prod-4.8.0.0"
+  kv_image_tag   = "prod-1.2.0.0"
   kms_url        = "https://depa-inferencing-kms.centralindia.cloudapp.azure.com"
 
 }
